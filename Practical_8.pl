380017e3e49ff43d69e7453872cd92edd7825d3f
Hrishit Dey <hrishitdey@gmail.com>
2025-11-27 12:36:45 +0530

    Implement power function in PROLOG

---
+++ Practical_8.pl
@@ -0,0 +1,11 @@
+%Write a PROLOG program to implement power (Num, Pow, Ans) : where Num is
+%raised to the power Pow to get Ans.
+% Base case:Any number raised to the power 0 is 1
+power(_, 0, 1) :- !.
+
+% Recursive case:Num^Pow = Num * Num^(Pow - 1)
+power(Num, Pow, Ans) :-
+    Pow > 0,
+    P1 is Pow - 1,
+    power(Num, P1, Temp),
+    Ans is Num * Temp.
