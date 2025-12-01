4343034b82b142c49cae8e0afcc695280138bce1
Hrishit Dey <hrishitdey@gmail.com>
2025-12-01 20:28:46 +0530

    implement memb(X, L)

--- Practical_10.pl
+++ Practical_10.pl
@@ -1,7 +1,7 @@
 %Write a PROLOG program to implement memb(X, L): to check whether X is a member
 %of L or not.
 % Base case: If the head of the list is X, then X is a member.
-memb(X, [X|_]) :- !.
+memb(X, [X|_]).
 
 % Recursive case: Else, check whether X is in the tail of the list.
 memb(X, [_|T]) :-
