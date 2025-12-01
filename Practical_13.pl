543cafb080485975faca311adb8df32149c71d4c
Hrishit Dey <hrishitdey@gmail.com>
2025-12-01 20:01:40 +0530

    maximum number in a list maxlist

--- Practical_13.pl
+++ Practical_13.pl
@@ -1,14 +1,15 @@
 %Write a PROLOG program to implement maxlist(L, M) so that M is the
 % maximum number in the list.
-
-% Base case: If the list has only one element, that element is the maximum.
+% Base case: single element
 maxlist([X], X).
 
-% Recursive case:
-% If the list has a head H and tail T, first find the maximum of the tail (MT),
-% then compare H with MT to find the maximum M.
-maxlist([H | T], M) :-
+% Case: head is greater or equal to max of tail
+maxlist([H|T], H) :-
     maxlist(T, MT),
-    (H > MT -> M = H
-    ; M = MT).
+    H >= MT.
+
+% Case: head is less than max of tail
+maxlist([H|T], MT) :-
+    maxlist(T, MT),
+    H < MT.
 
