52ffe9e375dcba9851827086ea342583f1536095
Hrishit Dey <hrishitdey@gmail.com>
2025-12-01 20:27:29 +0530

    implement power (Num, Pow, Ans)

--- Practical_8.pl
+++ Practical_8.pl
@@ -1,7 +1,7 @@
 %Write a PROLOG program to implement power (Num, Pow, Ans) : where Num is
 %raised to the power Pow to get Ans.
 % Base case:Any number raised to the power 0 is 1
-power(_, 0, 1) :- !.
+power(_, 0, 1).
 
 % Recursive case:Num^Pow = Num * Num^(Pow - 1)
 power(Num, Pow, Ans) :-
